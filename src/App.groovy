8b9c5dd4f44e76afd18782ca308ce6e644125a5f
light <vlight88@gmail.com>
2014-10-25 16:23:58 +0300

    Main app

--- src/App.groovy
+++ src/App.groovy
@@ -1,1 +1,11 @@
-println 'Hello I am groovy file'
+
+import org.joda.time.LocalTime;
+
+class App {
+  static void main(args) {
+    println 'main args...'
+      
+      LocalTime currentTime = new LocalTime();
+    System.out.println("The current local time is: " + currentTime);
+  }
+}
